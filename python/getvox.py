6f34ff2deb346ba12f0dc65b1ef9e16ef48030e2
Rob Spectre <rob.spectre@gmail.com>
2011-02-13 12:59:49 -0500

    1) Integrated gender checker. 2) Refined espeak voices.

--- python/getvox.py
+++ python/getvox.py
@@ -17,27 +17,30 @@ import sys
 import syllables
 import subprocess
 import hashlib
+import face_client
 
 '''Objects'''
 class TweetFilter:
-    '''          
+    '''
     Implementation
     text = "@dn0t I'll send that !@#$%#!@$girl home wit a smid-ile http://wtf.me/w00t"
     filter = TweetFilter(text)
     print filter.read()
     '''
-    def __init__(self, text):
+    def __init__(self, text, username):
         self.text = text
         self.output = self.filterTweet(text)
-        
+        self.syllables = Syllables(text)
+        self.user = self.getUser(username)
+
     def filterTweet(self, input):
         input = self.filterUris(input)
         input = self.filterReplies(input)
         input = self.filterCharacters(input)
-        
+
         output = input
         return output
-           
+
     def filterUris(self, input):
         if "http" in input:
             t = input[input.find("http://"):]
@@ -47,31 +50,54 @@ class TweetFilter:
             return output
         else:
             return input
-    
+
     def filterReplies(self, input):
         # Extract @reply
         if "@" in input:
             t = input[input.find("@"):]
             t = t[:t.find(" ")]
-        
+
             user = self.request("http://api.twitter.com/1/users/show.json?screen_name=" + t)
             if user:
                 output = input.replace(t, user['name'])
             return output
         else:
             return input
-    
+
     def filterCharacters(self, input):
         pattern = re.compile('[^\'\s\w_]+')
         output = pattern.sub('', input)
         return output
-    
+
     def filterObscenity(self, input):
         self.stub = True
-        
+
     def read(self):
-        return self.output
-    
+        return {
+                'text': self.output,
+                'user': self.user,
+                'syllables': self.syllables.read()
+        }
+
+    def getUser(self, username):
+        user = self.request("http://api.twitter.com/1/users/show.json?screen_name=" + username)
+        if not user:
+            return False
+
+        # Try to fetch gender
+        myDetector = GenderDetect()
+        uri = user['profile_image_url'].replace("_normal", "")
+        detection = myDetector.detect(uri)
+        if detection['status']:
+            try:
+                gender = detection['photos'][0]['tags'][0]['attributes']['gender']['value']
+            except KeyError:
+                gender = "neuter"
+        else:
+            gender = "neuter"
+
+        return {'name': user['name'], 'gender': gender}
+
     def request(self, path, format="json"):
         # Add debugging to post
         #h=urllib2.HTTPHandler(debuglevel=1)
@@ -84,19 +110,25 @@ class TweetFilter:
         except urllib2.HTTPError, e:
             print "Error!: " + str(e)
             return False
-        
+
         data = r.read()
-        
-        if format is "json":       
+
+        if format is "json":
             return json.loads(data)
         else:
             return data
 
 class Syllables:
+    '''
+    Implementation
+    text = "@dn0t I'll send that !@#$%#!@$girl home wit a smid-ile http://wtf.me/w00t"
+    syllables = Syllables(text)
+    print syllables.read()
+    '''
     def __init__(self, text):
         self.text = text
         self.output = self.countSyllables(text)
-    
+
     def countSyllables(self, input):
         list =[]
         for word in input.split(" "):
@@ -106,9 +138,13 @@ class Syllables:
                 "count": count
             })
         return list
-    
+
     def read(self):
         return self.output
+
+class GenderDetect(face_client.FaceClient):
+    def detect(self, uri):
+        return self.faces_detect(uri)
     
 class Espeak:
     def __init__(self, text, voice=5):
@@ -117,7 +153,7 @@ class Espeak:
     
     def say(self, text, voice):
         hash = hashlib.md5(text).hexdigest()
-        f = open("cache/" + str(hash) + ".wav", 'wb')
+        f = open("/tmp/" + str(hash) + ".wav", 'wb')
         p = subprocess.Popen(['espeak', '-v' + voice, '-s 130', '-p 40', '--stdout', text], stdout = f)
         p.wait()
         f.close()
@@ -131,9 +167,13 @@ class Espeak:
 if __name__ == "__main__":
     try:
         text = sys.argv[1]
-        voice = sys.argv[2]
+        user = sys.argv[2]
     except IndexError:
         print "Usage: getvox.py \"String you want vox for.\""
-    filter = TweetFilter(text)
-    espeak = Espeak(filter.read(), voice)
-    print "cache/" + espeak.read()
+    filter = TweetFilter(text,user).read()
+    if filter['user']['gender'] == "male":
+        voice = "klatt"
+    else:
+        voice = "female4"
+    espeak = Espeak(filter['text'], voice)
+    print "/tmp/" + espeak.read()
