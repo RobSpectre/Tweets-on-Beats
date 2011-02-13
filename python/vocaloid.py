4942e6e2ae6aab5d3570f9a0a980a227644a8dfe
Vincent Akkermans <vincent.akkermans@upf.edu>
2011-02-13 02:45:03 +0100

    making voice synth standalone

--- python/vocaloid.py
+++ python/vocaloid.py
@@ -1,14 +1,23 @@
-import subprocess, midi
-from canoris import Canoris, Template, Task
+import subprocess, midi, re, urllib2, json, uuid, os
+#from canoris import Canoris, Template, Task
 
 '''
 at the character where you're encoding:
     do the next 2 characters represent a phoneme together?
+
+CANORIS_URL = 'http://localhost'
+CANORIS_KEY = '867c069719dc45db930c7d9749de8659'
 '''
 
-CANORIS_URL = 'http://api.canoris.com'
-CANORIS_KEY = '867c069719dc45db930c7d9749de8659'
+TMP_DIR = '/tmp'
+NFS_BASEDIR = '/mnt/m30_local'
 
+PROCESSING_VOCALOID_DIR          = os.path.join(NFS_BASEDIR, 'vocaloid')
+PROCESSING_VOCALOID_DIR_CONFIG   = PROCESSING_VOCALOID_DIR.replace('/', '\\')
+WINE_ENVIRONMENT                 = '/home/v/vocaloid/environment'
+PROCESSING_VOCALOID_SOURCE_DIR   = os.path.join(PROCESSING_VOCALOID_DIR, 'bin')
+
+TMP_DIR = '/tmp'
 
 TRANSLATION_TABLE = {
         'p': 'p',               #
@@ -41,7 +50,7 @@ TRANSLATION_TABLE = {
         '@L': 'l',               #    simple
         '@2': 'D',               #    the    Used only for "the".
         '@5': 'tU',              #    to    Used only for "to".
-        'a': '{',               #    trap
+        'a': '@',               #    trap
         'a2': '@',               #    about    This may be '@'               # or may be a more open schwa.
         'A:': '@',               #    palm
         'A@': 'A@',               #    start
@@ -49,7 +58,7 @@ TRANSLATION_TABLE = {
         'e@': 'e@',               #    square
         'I': 'I',               #    kit
         'I2': 'I',               #    intend    As 'I'               #, but also indicates an unstressed syllable.
-        'i':  'i:',              #    happy    An unstressed "i" sound at the end of a word.
+        'i':  'I',              #    happy    An unstressed "i" sound at the end of a word.
         'i:': 'i:',               #    fleece
         'i@': 'I@',               #    near
         '0': 'Q',               #    lot
@@ -68,33 +77,34 @@ TRANSLATION_TABLE = {
         ' ': ' ',               # keep spaces!
                      }
 
-
+'''
 def init_canoris():
     Canoris.set_api_key(CANORIS_KEY)
-    Canoris.set_api_base(CANORIS_URL)
+    Canoris.set_base_uri(CANORIS_URL)
+    '''
 
 class VocaloidTask():
-    def __init__(self, text, midi_path):
+    def __init__(self, text, midi_path, bpm):
         self.midi_path = midi_path
         # drop in Rob's function here:
         filter = TweetFilter(text)
         self.text = filter.read()
         # end Rob's function
-        self.sequence = self.__generate_sequence(
-                            self.__espeak_to_vocaloid_phonemes(
-                                self.__get_espeak_output(text)),
-                                midi_path)
-
-
-        new_task = Task.create_task('vocaloid', {'voice': 'lara', 'sequence': self.sequence})
-        # we're going to check this later, because it won't be done right away.
-        self.__save_task(new_task)
+        events, ticklength = self.__process_midi_file(midi_path)
+        self.sequence = self.__generate_xml(self.__espeak_to_vocaloid_phonemes(
+                                                self.__get_espeak_output(self.text)),
+                                            events,
+                                            1.0/ticklength)
+        print self.sequence
+        self.xml_file = self.__save_tmp_file(self.sequence)
+        self.wav_file = self.__generate_audio(self.xml_file)
+        print self.wav_file
 
     @staticmethod
     def __get_espeak_output(input):
         p = subprocess.Popen(['espeak', '-x', '-q', input], stdout=subprocess.PIPE)
         p.wait()
-        return p.communicate()[0]
+        return p.communicate()[0].replace("'", "")
 
     @staticmethod
     def __espeak_to_vocaloid_phonemes(input):
@@ -128,7 +138,7 @@ class VocaloidTask():
         return result
 
     @staticmethod
-    def process_midi_file(midi_path):
+    def __process_midi_file(midi_path):
         midifile = midi.MidiFile()
         midifile.open(midi_path)
         midifile.read()
@@ -139,14 +149,12 @@ class VocaloidTask():
         last_time = 0
         note_closed_p = True
         for event in track.events:
-            print event.time
-            if event.type == 'NOTE_OFF' and not note_closed_p :
+            if event.type == 'NOTE_OFF' or (event.type == 'NOTE_ON' and not note_closed_p):
                 # make sure we don't run into an exception if the midi file
                 # is malformed (e.g. doesn't start with a noteon)
                 if len(events) > 0:
-                    events[-1]['d'] = event.time - last_time
-                    last_time = event.time
-                    note_closed_p = True
+                    events[-1]['d'] = float(event.time - last_time) #float(midifile.ticksPerQuarterNote)
+                note_closed_p = True
                 last_time = event.time
             if event.type == 'NOTE_ON':
                 last_time = event.time
@@ -160,10 +168,53 @@ class VocaloidTask():
                     events[-1]['r'] = True
                 note_closed_p = False
                 events.append(new_note)
-        return events
+        return events, midifile.ticksPerQuarterNote
+
+    @staticmethod
+    def __save_tmp_file(sequence):
+        p = os.path.join(TMP_DIR, str(uuid.uuid4()))
+        f = open(p, 'w')
+        f.write(sequence)
+        f.close()
+        return p
+
+    @staticmethod
+    def __generate_xml(syllables, melody, ticklength):
+        sequence = "<melody ticklength='%s'>" % ticklength
+        str_syllables = [' '.join(x) for x in syllables]
+        syl_i = 0
+        mel_i = 0
+        while syl_i < len(str_syllables):
+            mel_i = mel_i % len(melody)
+            if melody[mel_i]['r']:
+                sequence += "<rest duration='%s'/>" % melody[mel_i]['d']
+            else:
+                sequence += "<note duration='%s' pitch='%s' velocity='%s' phonemes='%s'/>" % \
+                                (melody[mel_i]['d'], melody[mel_i]['p'], melody[mel_i]['v'], ' '.join(syllables[syl_i]))
+                syl_i += 1
+            mel_i += 1
+
+        sequence += "</melody>"
+        return sequence
+
+    @staticmethod
+    def __generate_audio(input_file):
+        output_file = p = os.path.join(TMP_DIR, str(uuid.uuid4())+'.wav')
+        os.chdir(WINE_ENVIRONMENT)
+        executable_full = os.path.join(WINE_ENVIRONMENT, 'VocaloidServer.exe')
+
+        # execute the application
+        exec_array = ['wine', executable_full, '--in', input_file, '--out', output_file, '--norm']
+        p = subprocess.Popen(exec_array, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env={'WINEPREFIX': WINE_ENVIRONMENT})
+        p_result = p.wait()
+        # if the return_code is not 0, it should mean an error ocurred
+        if p_result != 0:
+            output_std, output_err = p.communicate()
+            raise Exception("ProcessingError", 'error: ' + str(p_result) + ' ' + str(output_std) + "\n" + str(output_err))
+        return output_file
 
 class TweetFilter:
-    '''          
+    '''
     Implementation
     text = "@dn0t I'll send that !@#$%#!@$girl home wit a smid-ile http://wtf.me/w00t"
     filter = TweetFilter(text)
@@ -172,15 +223,15 @@ class TweetFilter:
     def __init__(self, text):
         self.text = text
         self.output = self.filterTweet(text)
-        
+
     def filterTweet(self, input):
         input = self.filterUris(input)
         input = self.filterReplies(input)
         input = self.filterCharacters(input)
-        
+
         output = input
         return output
-           
+
     def filterUris(self, input):
         if "http" in input:
             t = input[input.find("http://"):]
@@ -190,31 +241,31 @@ class TweetFilter:
             return output
         else:
             return input
-    
+
     def filterReplies(self, input):
         # Extract @reply
         if "@" in input:
             t = input[input.find("@"):]
             t = t[:t.find(" ")]
-        
+
             user = self.request("http://api.twitter.com/1/users/show.json?screen_name=" + t)
             if user:
                 output = input.replace(t, user['name'])
             return output
         else:
             return input
-    
+
     def filterCharacters(self, input):
         pattern = re.compile('[^\'\s\w_]+')
         output = pattern.sub('', input)
         return output
-    
+
     def filterObscenity(self, input):
         self.stub = True
-        
+
     def read(self):
         return self.output
-    
+
     def request(self, path, format="json"):
         # Add debugging to post
         #h=urllib2.HTTPHandler(debuglevel=1)
@@ -227,24 +278,17 @@ class TweetFilter:
         except urllib2.HTTPError, e:
             print "Error!: " + str(e)
             return False
-        
+
         data = r.read()
-        
-        if format is "json":       
+
+        if format is "json":
             return json.loads(data)
         else:
             return data
 
 
-'''
-
-
-
-'''
-
-
 if __name__ == '__main__':
-    events = VocaloidTask.process_midi_file('/home/v/Sounds/Godfather.mid')
-    print [x['p'] for x in events]
-    print [x['d'] for x in events]
+    #init_canoris()
+    vt = VocaloidTask('This is a tweet', '/home/v/Sounds/Godfather.mid', 1)
+
 
