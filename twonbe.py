f584e8958db3c957c954e5590dc41c5fb77a9bd8
Rob Spectre <rob.spectre@gmail.com>
2011-03-13 18:09:58 -0400

    1) Removed interval polling - Daemon now goes as fast as it can. 2) Fixed PollTwitter job to keep track of lastProcessedId. 3) Made jobs Thread objects - they now run in total parallel. 4) Looped in FilterTweet Job.

--- twonbe.py
+++ twonbe.py
@@ -11,17 +11,17 @@ import logging
 import urllib
 import urllib2
 import simplejson
-import memcache
+import redis
+import datetime
 
 '''
 Configuration variables
 '''
 searchapi = "http://search.twitter.com"
 keyword = "beatify"
-memcacheHosts = ['localhost:11211']
-loggingLevel = logging.DEBUG
+logging_level = logging.DEBUG
 logging.basicConfig()
-
+redis_host = "localhost"
 
 '''
 Main Daemon
@@ -31,7 +31,6 @@ class TwonbeDaemon(threading.Thread):
     def __init__(self, interval=10):
         self.elapsed = 0
         self.fault = False
-        self.timer = threading.Event()
         self.interval = interval
         self.util = Utility() 
         self.jobs = []
@@ -39,34 +38,36 @@ class TwonbeDaemon(threading.Thread):
 
         # Configuration directives
         self.log = logging.getLogger("TwonbeDaemon")  
-        global loggingLevel
-        self.log.setLevel(loggingLevel)
+        global logging_level
+        self.log.setLevel(logging_level)
         self.log.info("Initialized.")
 
     def run(self):
         # Main loop
         self.log.info("Launching Twonbe Daemon.")
-        while self.fault is False:
-            self.log.debug("Elapsed time: %s" % (str(self.elapsed)))
-            try:
-                self.process()
-            except Exception as e:
-                raise TwonbeError(e, "Error encountered processing job.")
-            self.check()
-            self.timer.wait(self.interval)
-            self.elapsed = self.elapsed + self.interval
+        try:
+            while self.fault is False:
+                try:
+                    self.process()
+                except Exception as e:
+                    raise TwonbeError(e, "Error encountered processing job.")
+                #self.check()
+                self.elapsed = self.elapsed + self.interval
+        except KeyboardInterrupt:
+            print "\nDone!"
 
     def process(self):
         # Process job queue
-        global loggingLevel
-        if loggingLevel == logging.DEBUG:
+        global logging_level
+        if logging_level == logging.DEBUG and self.jobs:
             self.log.debug("Job queue: %s" % (str(len(self.jobs))))
         if self.jobs:
             for job in self.jobs:
                 try:
                     self.log.info("Processing job: %s" % (job.name))
-                    job.process()
                     job.elapsed = 0
+                    job.start()
+                    self.removeJob(job)
                 except Exception as e:
                     self.stop()
                     raise TwonbeError(e, "encountered error with job: %s" % (job.name))
@@ -74,11 +75,18 @@ class TwonbeDaemon(threading.Thread):
     def addJob(self, job):
         self.log.debug("Adding new %s job: %s" % (job.name, job.id))
         try:
-            self.jobs
             self.jobs.append(job)
         except Exception as e:
             raise TwonbeError(e, "encountered error adding %s job: %s" % (job.name, job.id))
         return self.log.debug("Added new %s job: %s" % (job.name, job.id))
+    
+    def removeJob(self, job):
+        self.log.debug("Removing %s job: %s" % (job.name, job.id))
+        try:
+            self.jobs.remove(job)
+        except Exception as e:
+            raise TwonbeError(e, "encountered error adding %s job: %s" % (job.name, job.id))
+        return self.log.debug("Removed %s job: %s" % (job.name, job.id))
     
     def stop(self):
         self.log.debug("Stop command received.")
@@ -96,31 +104,38 @@ Object for Jobs
 
 Extend this object and add to TwonbeDaemon object to queue and process.
 '''
-class Job(object):
-    def __init__(self, name, id, interval=10):
+class Job(threading.Thread):
+    def __init__(self, name, id, interval=None):
+        threading.Thread.__init__(self)
         self.name = name   
         self.id = id
         self.interval = interval
         self.elapsed = 0
         self.util = Utility()
+        self.timer = threading.Event()
         global twonbe
         self.twonbe = twonbe
         
         # Configuration directives
         self.log = logging.getLogger(name)
-        global loggingLevel
-        self.log.setLevel(loggingLevel)
+        global logging_level
+        self.log.setLevel(logging_level)
         self.log.debug("Created %s job: %s" % (self.name, self.id))
     
     def run(self):
         # Error-safe wrapper for user-defined job logic
+        if self.interval:
+            self.log.debug("Starting job in %s seconds." % (str(self.interval)))
+            self.timer.wait(self.interval)
         self.log.debug("Starting %s job: %s" % (self.name, self.id))
         try:
             self.log.info("Processing %s job: %s" % (self.name, self.id))
             self.process()
             self.log.debug("Finished %s job: %s" % (self.name, self.id))
+        except KeyboardInterrupt:
+            print "\nDone!"
         except Exception as e:
-            raise TwonbeError(e, "%s failed:" % (job.name))
+            raise TwonbeError(e, "%s: %s failed:" % (self.name, self.id))
         
     def process(self):
         # Extend this function to initiate job logic.
@@ -133,10 +148,36 @@ These are the individual jobs chained together to convert a tweet into a Twonbe
 '''
 class PollTwitter(Job):
     def __init__(self, id, lastProcessedId=None):
+        self.id = id
         self.lastProcessedId = lastProcessedId
-        Job.__init__(self, "PollTwitter", id)
+        Job.__init__(self, "PollTwitter", id, 30)
     
     def process(self):
+        self.log.debug("Starting to process polling job.")
+        path = self.buildRequest()
+        if path:
+            json = self.util.request(path)
+        else:
+            raise TwonbeError("Error polling", "Could not build request path.")
+        
+        if json:
+            self.log.debug("Parsing JSON object.")
+            data = simplejson.loads(json)
+        else:
+            raise TwonbeError("Error polling", "Could not parse JSON object.")
+        
+        if data:
+            for tweet in data['results']:
+                self.log.debug("Queueing up tweet to be checked: %s" % (tweet['id_str']))
+                self.twonbe.addJob(CheckTweet(tweet['id_str'], tweet))
+                lastProcessedId = tweet['id_str']
+            self.log.debug('Completed poll - scheduling next one.')
+            return self.twonbe.addJob(PollTwitter(str(int(self.id) + 1), lastProcessedId))
+        else:
+            self.log.debug('Found no data in this job - scheduling next one.')
+            return self.twonbe.addJob(PollTwitter(str(int(self.id) + 1), self.lastProcessedId))
+    
+    def buildRequest(self):
         # Build polling request
         self.log.debug("Building polling request.")
         global searchapi
@@ -145,22 +186,69 @@ class PollTwitter(Job):
             params = {
                 'q': keyword,
                 'result_type': "recent",
-                'since_id': lastProcessedId
+                'since_id': self.lastProcessedId
             }
         else:
             params = {
                 'q': keyword,
                 'result_type': "recent"
             }
-        path = searchapi + "/search.json?" + urllib.urlencode(params)
-        json = self.util.request(path)
-        self.log.debug("Parsing JSON object.")
-        data = simplejson.loads(data)
-        self.log.debug("Returned JSON: %s" % (str(data)))
+        return searchapi + "/search.json?" + urllib.urlencode(params)
+
+class CheckTweet(Job):
+    def __init__(self, id, tweet):
+        self.id = id
+        self.tweet = tweet
+        self.limit = 600
+        Job.__init__(self, "FilterTweet", id)
+        
+    def process(self):
+        if self.isNotEnglish() or self.isOld() or self.isAttempted():
+            self.log.debug("Tweet does not pass initial checks - skipping.")
+            return False
+        else:
+            self.log.info("Tweet passes initial checks - filtering.")
+            return self.twonbe.addJob(FilterTweet(self.id, self.tweet))
+    
+    def isAttempted(self):
+        self.log.debug("Checking if tweet has been attempted before...")
+        if self.util.redis.sadd("attempted", self.tweet['id_str']):
+            self.log.debug("Tweet has not been attempted before..")
+            return False
+        else:
+            self.log.debug("Tweet has been attempted before. Skipping.")
+            return True
+    
+    def isOld(self):
+        self.log.debug("Checking if tweet is too older than %s seconds." % (str(self.limit)))
+        now = datetime.datetime.utcnow()
+        timestamp = self.tweet['created_at'].replace(" +0000", "")
+        then = datetime.datetime.strptime(timestamp, "%a, %d %b %Y %H:%M:%S")
+        delta = now - then
+        if delta.seconds < self.limit:
+            self.log.debug("Tweet is not too old.")
+            return False
+        else:
+            self.log.debug("Tweet is too old. Skipping.")
+            return True
+    
+    def isNotEnglish(self):
+        self.log.debug("Checking if tweet is in English.")
+        if self.tweet['iso_language_code'] == "en":
+            self.log.debug("Tweet is in English.")
+            return False
+        else:
+            self.log.debug("Tweet is not in English. Skipping.")
+            return True
 
 class FilterTweet(Job):
-    def __init__(self, id):
+    def __init__(self, id, tweet):
+        self.id = id
+        self.tweet = tweet
         Job.__init__(self, "FilterTweet", id)
+    
+    def process(self):
+        self.log.debug("Processing this shit!")
     
 class GenderCheck(Job):
     def __init__(self, id):
@@ -182,6 +270,9 @@ class TweetTwonbe(Job):
     def __init__(self, id):
         Job.__init__(self, "TweetTwonbe", id)
 
+class CleanupTwonbe(Job):
+    def __init__(self, id):
+        Job.__init__(self, "TweetTwonbe", id)
 
 '''
 Utility functions
@@ -190,14 +281,15 @@ class Utility:
     def __init__(self):
         # Configuration directives
         self.log = logging.getLogger("Utility")
-        global loggingLevel
-        self.log.setLevel(loggingLevel)
-        if loggingLevel == logging.DEBUG:
+        self.hash = None
+        global logging_level
+        self.log.setLevel(logging_level)
+        if logging_level == logging.DEBUG:
             logger = 1
         else:
             logger = 0
-        global memcacheHosts
-        self.mc = memcache.Client(memcacheHosts, debug=logger)
+        global redis_host
+        self.redis = redis.Redis(redis_host)
     
     def timestr(self, seconds):
         if seconds >= 24*60*60:
@@ -215,8 +307,8 @@ class Utility:
     
     def request(self, path):
         # Add debugging to post if logging level is debug
-        global loggingLevel
-        if loggingLevel == logging.DEBUG:
+        global logging_level
+        if logging_level == logging.DEBUG:
             h=urllib2.HTTPHandler(debuglevel=1)
             opener = urllib2.build_opener(h)
             urllib2.install_opener(opener)
@@ -230,23 +322,30 @@ class Utility:
         self.log.debug("URI retrieved: %s" % (path))
         data = r.read()   
         return data
+     
 
 '''
 Twonbe Exception
 '''
 class TwonbeError(Exception):
-    def __init__(self, e, m):
+    def __init__(self, message, e):
+        Exception.__init__(self, message)
         # Configuration directives
         self.log = logging.getLogger("TwonbeError")
-        global loggingLevel
-        self.log.setLevel(loggingLevel)
-        self.log.error(self.errorMessage(e, m))
+        global logging_level
+        self.log.setLevel(logging_level)
+        if logging_level == logging.DEBUG:
+            self.errorOut(message, e)
+        else:
+            self.errorLog(message, e)
     
-    def errorMessage(self, e, m):
-        return "%s! %s" % (m, self.args[0])
+    def errorLog(self, message, e):
+        self.log.error("%s! %s" % (str(e), message))
+        pass
         
-   
-    
+    def errorOut(self, message, e):
+        return self.log.error("%s! %s" % (str(e), message))
+            
 '''
 Implementation
 '''
